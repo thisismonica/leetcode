34c0e3de8638e2f98600b6bd731a7c9968798b78
wqr619 <qirongismonica@gmail.com>
2015-01-07 21:47:47 -0800

    Add comment

--- word_break.py
+++ word_break.py
@@ -10,10 +10,14 @@ class Solution:
             return True
         dp = [False for i in range(N)]
 
+        # Dynamic Programming
+        # dp[i] == s[:i+1] is word break
         for i in range(N):
+            # if itself is a word, then its True
             if s[:i+1] in dict:
                 dp[i] = True
             else:
+                # find k that previous k is word break, (k-i) is a word
                 for k in range(0,i):
                     if dp[k] and s[k+1:i+1]in dict:
                         dp[i] = True
