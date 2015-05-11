a3b36d6648882db0d1c508fe8fb33cd462bad8ac
wqr619 <qirongismonica@gmail.com>
2015-05-11 15:00:49 -0700

    Better DP solution with constant space

--- climb_stairs.py
+++ climb_stairs.py
@@ -1,18 +1,15 @@
 class Solution:
-    # @param n, an integer
-    # @return an integer
+    # @param {integer} n
+    # @return {integer}
     def climbStairs(self, n):
-        if n == 0:
-            return 0
+        if n < 3 :
+            return n
         
-        if n == 1:
-            return 1
-        
-        dp = [0 for i in range(n+1)]
-        dp[0] = 1
-        dp[1] = 1
-        
-        for i in range(2,n+1):
-            dp[i] = dp[i-1] + dp[i-2]
-        
-        return dp[-1]
+        ways_prev_prev = 1
+        ways_prev = 2
+        for i in range(3, n+1):
+            ways = ways_prev + ways_prev_prev
+            ways_prev_prev = ways_prev
+            ways_prev = ways
+        return ways
+
