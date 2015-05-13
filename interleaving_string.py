658e38179c2c3d10b5f25cc8c6779238655d651c
wqr619 <qirongismonica@gmail.com>
2015-05-12 20:38:45 -0700

    DP solution for interleaving string

--- interleaving_string.py
+++ interleaving_string.py
@@ -1,33 +1,24 @@
 class Solution:
-    # @return a boolean
+    # @param {string} s1
+    # @param {string} s2
+    # @param {string} s3
+    # @return {boolean}
     def isInterleave(self, s1, s2, s3):
-        if len(s3) != len(s1) + len(s2):
+        l1, l2, l3 = len(s1), len(s2), len(s3)
+        if l3 != l1+l2:
             return False
-        if len(s3) == 0:
-            return True
-            
-        p1, p2, p3 = 0,0,0
+        dp = [ [False for i in range(l2+1)] for j in range(l1+1)]
+        dp[0][0] = True
+        for i in range(1,l1+1):
+            dp[i][0] = s1[i-1]==s3[i-1] and dp[i-1][0]
+        for j in range(1,l2+1):
+            dp[0][j] = s2[j-1]==s3[j-1] and dp[0][j-1]
         
-        while p3 != len(s3):
-            if p1 == len(s1):
-                if s3[p3] == s2[p2]: p2 = p2 + 1 ; p3 = p3 + 1; continue
-                else: return False
-            if p2 == len(s2):
-                if s3[p3] == s1[p1] : p1 = p1 + 1 ; p3 = p3 + 1; continue
-                else: return False
-            if s3[p3] == s1[p1] and s3[p3] == s2[p2]:
-                return self.isInterleave(s1[p1+1:], s2[p2:], s3[p3+1:]) or self.isInterleave(s1[p1:],s2[p2+1:],s3[p3:])
-            elif s3[p3] == s1[p1]:
-                p1 = p1 + 1
-            elif s3[p3] == s2[p2]:
-                p2 = p2 + 1
-            else:
-                return False
-            p3 = p3 + 1
-        return True
+        for i in range(1,l1+1):
+            for j in range(1,l2+1):
+                if s1[i-1]==s3[i+j-1]:
+                    dp[i][j] = dp[i-1][j]
+                if s2[j-1]==s3[i+j-1]:
+                    dp[i][j] = dp[i][j] or dp[i][j-1]
 
-s = Solution()
-s1 = "aa"
-s2 = "ab"
-s3 = "aaba"
-print s.isInterleave(s1,s2,s3)
+    return dp[l1][l2]
