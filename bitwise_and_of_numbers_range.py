88fa812d5b4c4a24a000d5aa07b671c2b9fe1990
wqr619 <qirongismonica@gmail.com>
2015-07-17 23:52:51 -0700

    07/17

---
+++ bitwise_and_of_numbers_range.py
@@ -0,0 +1,11 @@
+class Solution:
+    # @param {integer} m
+    # @param {integer} n
+    # @return {integer}
+    def rangeBitwiseAnd(self, m, n):
+        p = 0
+        while m!=n:
+            m >>= 1
+            n >>= 1
+            p += 1
+        return m << p
